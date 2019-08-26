67798ad20fe1cef3790146b1e55a2c4db6bf24be
ConnorMeehan <connorgmeehan23@gmail.com>
2019-08-27 00:52:15 +1000

    Switched typography to inter and anonymous pro

--- src/components/HeroHistory.vue
+++ src/components/HeroHistory.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="History">
         <div class="History_Element" v-for="(workplace, i) in history" :key="i">
-            <h5 class="History_Date">{{workplace.startmonth}}<br>{{workplace.startyear}}</h5>
+            <h5 v-if="i < history.length - 1"
+             class="History_Date">{{new dateFormatter(workplace.startDate).getMonth()}}<br>{{new dateFormatter(workplace.startDate).getYear()}}</h5>
+            <h5 v-if="i >= history.length - 1"
+             class="History_Date">TBD<br>{{new dateFormatter(new Date()).getYear()}}</h5>
             <div class="History_MainBlock">
                 <h3 class="History_Title">{{workplace.title}}</h3>
                 <a v-if="workplace.link" :href="workplace.link">
@@ -14,23 +17,23 @@
 </template>
 
 <script>
+import DateFormatter from '~/helpers/DateFormatter';
 export default {
     data: function() {
         return {
+            dateFormatter: DateFormatter,
             history: [
                 {
                     title: "Software Development Tutor",
                     workplace: "Think Table",
                     link: "https://www.facebook.com/tutorreactive/",
-                    startmonth: "Feb",
-                    startyear: "2016"
+                    startDate: "01-02-2016",
                 },
                 {
                     title: "Junior Frontend Dev",
                     workplace: "Pacific Magazines",
                     link: "http://www.pacificmags.com.au/",
-                    startmonth: "Jul",
-                    startyear: "2018"
+                    startDate: "01-06-2018",
                 },
                 {
                     title: "Junior Creative Technologist",
