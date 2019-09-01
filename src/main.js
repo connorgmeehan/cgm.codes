8d672bb68932eae5731af85d23a904ca0e96b845
ConnorMeehan <connorgmeehan23@gmail.com>
2019-09-01 17:29:27 +1000

    fix on previous

--- src/main.js
+++ src/main.js
@@ -5,6 +5,36 @@ import './styles/main.scss';
 
 import DefaultLayout from '~/layouts/Default.vue'
 
+let animatedFavicon, perlinBackground;
+
+const getPerlinBackgroundTarget = () => {
+  return document.getElementById('Hero_CanvasContainer');
+}
+
+if(process.isClient) {
+  window.addEventListener('load', (e) => {
+    const AnimatedFavicon = require('./classes/AnimatedFavicon').default;
+    const animatedFavicon = new AnimatedFavicon('animated_favicon.json');
+  
+    // import PerlinBackground, { PerlinBackgroundSettings } from '../classes/PerlinBackground';
+    const PerlinBackground = require('./classes/PerlinBackground').default;
+    const PerlinBackgroundSettings = require('./classes/PerlinBackground').PerlinBackgroundSettings;
+  
+    const perlinTarget = getPerlinBackgroundTarget();
+    const perlinBackgroundSettings = PerlinBackgroundSettings;
+    perlinBackgroundSettings.padding = 50;
+  
+    console.log(perlinTarget, perlinBackgroundSettings);
+  
+    if (perlinTarget) {
+      perlinBackground = new PerlinBackground(perlinTarget, perlinBackgroundSettings);
+      console.log(perlinBackground);
+    }
+  });
+}
+
+
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
