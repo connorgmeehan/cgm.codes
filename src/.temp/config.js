fa7e6485be87c4d3906180e275f856c370c23792
ConnorMeehan <connorgmeehan23@gmail.com>
2019-10-06 15:11:14 +1100

    Removed tracking on .temp files

--- src/.temp/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "siteUrl": "https://cgm.codes/",
-  "siteName": "connor guy meehan codes, allegedly",
-  "titleTemplate": "%s - connor guy meehan codes, allegedly",
-  "siteDescription": "Personal portfolio of works by Connor Guy Meehan, Computing Design student at The University of Sydney and aspiring Creative Technologist!",
-  "version": "0.6.7"
-}
