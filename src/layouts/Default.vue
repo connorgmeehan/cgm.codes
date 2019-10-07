3274e9eb7bccd6cc28c7b2985319c3b28b43b33e
Connor Meehan <connorgmeehan23@gmail.com>
2019-10-07 18:57:45 +1100

    Removed email copying

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="Layout">
     <slot />
-
     <header class="Header_Wrapper">
       <div class="Header">
         <div class="Header_TextBlock">
@@ -11,44 +10,12 @@
         </div>
       </div>
     </header>
-    
     <div class="Footer" />
-
-    <div
-      id="CopySandbox_Notification"
-      v-on:click="closeCopyEmail"
-      class="CopySandbox_Wrapper">
-      <input
-        id="CopySandbox_Input"
-        :value="$static.metadata.contactEmail"
-        type="text">
-      <div class="CopySandbox">
-        <div class="CopySandbox_Content">
-          {{ $static.metadata.contactEmail }} saved to clipboard
-        </div>
-        <div class="CopySandBox_Close" />
-      </div>
-    </div>
   </div>
 </template>
 <script>
 export default {
   name: "Layout",
-  methods: {
-    copyEmail: function () {
-      const textbox = document.getElementById("CopySandbox_Input");
-      textbox.select();
-      textbox.value = this.$static.metadata.contactEmail;
-      document.execCommand("copy");
-
-      const notification = document.getElementById("CopySandbox_Notification");
-      notification.classList.add('CopySandbox_Wrapper__display');
-    },
-    closeCopyEmail: function () {
-      const notification = document.getElementById("CopySandbox_Notification");
-      notification.classList.remove('CopySandbox_Wrapper__display');
-    }
-  }
 }
 </script>
 <static-query>
