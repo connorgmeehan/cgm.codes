9f24ae62c8f13137eb32c8516ee8acf50070c4c7
Connor Meehan <connorgmeehan23@gmail.com>
2019-11-11 23:33:43 +1100

    Added face d_tect portfolio entryh

---
+++ src/components/EmbeddedYoutube.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="EmbeddedYoutube">
+      <iframe :src="src" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+</template>
+
+<script>
+import {capitaliseFirstLetter} from '../helpers/utils';
+const baseClass = 'EmbeddedYoutube';
+
+export default {
+  name: 'EmbeddedYoutube',
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      validator: (prop) => [
+          'fullwidth',
+          'halfwidth',
+      ].includes(prop),
+      default: 'fullwidth',
+    },
+  },
+  methods: {
+    getTypeModifier: function() { return `${baseClass}__${capitaliseFirstLetter(this.type)}` },
+  }
+}
+</script>
