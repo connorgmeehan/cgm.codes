c1efdcbd2280721917245c506bdc5d8172b37fae
Connor G Meehan <connorgm@pm.me>
2023-10-27 01:12:31 +1100

    wip CV

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            colors: {
+                'cream': {
+                    '50': '#fefde8',
+                    '100': '#fffdd0',
+                    '200': '#fff688',
+                    '300': '#ffea44',
+                    '400': '#fed811',
+                    '500': '#eebf04',
+                    '600': '#cd9401',
+                    '700': '#a46904',
+                    '800': '#87520c',
+                    '900': '#734310',
+                    '950': '#432305',
+                },
+            }
+        },
 	},
 	plugins: [],
 }
