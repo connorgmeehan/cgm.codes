5e1b666e45db526e818b24bffbe9ad0bc99569ff
Connor Meehan <connorgm@pm.me>
2024-04-10 09:46:43 +1000

    tweak: Adjust gray color scheme and switch to dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: 'class',
 	theme: {
 		extend: {
             colors: {
@@ -16,6 +17,34 @@ export default {
                     '800': '#87520c',
                     '900': '#734310',
                     '950': '#432305',
+                },
+                'gray': {
+                  DEFAULT: '#8B8467',
+                  50: '#F8F8F6',
+                  100: '#ECEAE6',
+                  200: '#D5D0C6',
+                  300: '#BDB6A5',
+                  400: '#A59D85',
+                  500: '#8B8467',
+                  600: '#6E6951',
+                  700: '#504E3C',
+                  800: '#333226',
+                  900: '#161610',
+                  950: '#070705'
+                },
+                'corn': {
+                  DEFAULT: '#9A8D58',
+                  50: '#F9F9F5',
+                  100: '#EFEFE3',
+                  200: '#DBD9C0',
+                  300: '#C6C29C',
+                  400: '#B2A978',
+                  500: '#9A8D58',
+                  600: '#796C46',
+                  700: '#594E33',
+                  800: '#393020',
+                  900: '#18140E',
+                  950: '#080705'
                 },
             }
         },
